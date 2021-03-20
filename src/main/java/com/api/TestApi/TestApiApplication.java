f01808a784460493c2e202ea05f97dfa1f53fba8
biscoffacoder <biscoffacoder@yahoo.com>
2021-03-20 17:01:52 +0530

    Swagger configuration done (localhost:8080/swagger-ui.html)

--- src/main/java/com/api/TestApi/TestApiApplication.java
+++ src/main/java/com/api/TestApi/TestApiApplication.java
@@ -8,7 +8,13 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class TestApiApplication {
 
 	public static void main(String[] args) {
@@ -22,6 +28,11 @@ public class TestApiApplication {
 	            .setReadTimeout(Duration.ofMillis(3000))
 	            .build();
 	}
+	@Bean
+	   public Docket productApi() {
+	      return new Docket(DocumentationType.SWAGGER_2).select()
+	         .apis(RequestHandlerSelectors.basePackage("com.api.TestApi")).build();
+	   }
 	
 
 }
